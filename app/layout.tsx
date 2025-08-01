c69f08e457663a4fd0e5e7b2fbc4dc5c5ee98443
Pauline Sherice Wee <paulinekowee@gmail.com>
2025-08-02 02:48:51 +0800

    fixed bush

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,27 @@
-import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
-import { Martian_Mono } from "next/font/google"
+import type React from "react";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Martian_Mono } from "next/font/google";
 
 const martianMono = Martian_Mono({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-  subsets: ['latin'],
-  variable: '--font-martian'
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-martian",
+});
 
 export const metadata: Metadata = {
-  title: "Digital Flower Bouquet",
+  title: "digibouquet",
   description: "Create and share a digital flower bouquet for Valentine's Day",
-  generator: 'v0.dev'
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={martianMono.variable}>
       <body className="font-martian">{children}</body>
     </html>
-  )
+  );
 }
